12514bcf80e3a54f6abe6b169432a98edf55277e
AlejandroRiba <alejandrorib0123@gmail.com>
2024-04-22 17:40:44 -0600

    Ajuste de pantallas, botones en pantalla principal

--- app/src/main/java/com/example/appmedica/MainActivity.kt
+++ app/src/main/java/com/example/appmedica/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.appmedica
 import android.os.Bundle
 import android.widget.Button
 import android.content.Intent
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +20,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val textView: TextView = findViewById(R.id.bienvenida)
+
+        val calendar = Calendar.getInstance()
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+
+        val texto: String = when {
+            hour < 12 -> "Buenos días, [Nombre]"
+            hour < 18 -> "Buenas tardes, [Nombre]"
+            else -> "Buenas noches, [Nombre]"
+        }
+        textView.text = texto
+
         val btn: Button = findViewById(R.id.agrmed)
         btn.setOnClickListener{
             val intent = Intent(this, MedicamentosActivity::class.java)
